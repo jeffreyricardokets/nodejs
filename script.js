928d894662c55b80656c45c7c1339fd54f92c2d0
jeffreyricardokets <93093426+jeffreyricardokets@users.noreply.github.com>
2021-11-24 09:36:00 +0100

    update

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("Hello Winc Academy students")
+//this is a larg sum this is a comment ;)
+/* console.log("1230941 x 1823794 =", 1230941 * 1823794)
+console.log("637263 / 54 = ", 637263 / 54)
+ */
